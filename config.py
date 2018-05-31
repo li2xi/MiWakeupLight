e384280240a9b9288e535d0b5a98bc09dc53ddce
li2xi <leetwoxi@gmail.com>
2018-06-01 02:33:41 +0800

    tmp

---
+++ config.py
@@ -0,0 +1,15 @@
+configs = {
+    "lights": [
+        {"ip": "192.168.2.214",
+         "token": "e74a4db55b549ffeecb8793ae2e826d4"},      #32bit token
+        {"ip": "192.168.2.156",
+         "token": "58253b66ee483062c9e758fbb6e58bd0"},
+        {"ip": "192.168.2.179",
+         "token": "8543a7a24cd3a66c30c834f082c121b2"},
+    ],
+    "wake_up_start_time": "7:00",
+    "wake_up_action": {
+        "mode": "linear",
+        "duration":  60
+    }
+}
---
+++ main.py
@@ -0,0 +1,58 @@
+import time
+import datetime
+import logging
+from miio.philips_bulb import PhilipsBulb
+from config import configs
+
+
+
+
+def get_lights():
+    lights = []
+    for l_config in configs["lights"]:
+        ip = l_config["ip"]
+        token = l_config['token']
+        light = PhilipsBulb(ip=ip, token=token, lazy_discover=False)
+        lights.append(light)
+    return lights
+
+
+def wakeup_entity():
+    wakeup_action = configs["wake_up_action"]
+    if wakeup_action["mode"] == "linear":
+        linear_wakeup(wakeup_action)
+    else:
+        raise ValueError("Unsupported mode: %s".format(wakeup_action["mode"]))
+
+def linear_wakeup(config=None):
+    logging.debug("linear mode.")
+    if config is None:
+        config = {}
+    lights = get_lights()
+    wake_up_duration = config["duration"]
+    for i in range(1, 101):
+        for light in lights:
+            light.on()
+            light.set_brightness_and_color_temperature(i, i)
+            logging.debug("set light %s's brightness and cct to %d", light.ip, i)
+        time.sleep(wake_up_duration / 100)
+
+def schedule_next_wakeup():
+    time.time()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level="DEBUG")
+    # lights = get_lights()
+    # for light in lights:
+    #     light.off()
+    # wakeup_entity()
+    t = time.strptime("03:00", "%H:%M")
+
+    print(time.localtime())
+    print(t)
+
+    print(time.localtime())
+    print(time.gmtime())
+    d1 = datetime.datetime.now()
+    print(d1)
